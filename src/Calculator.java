08e98be691e5ad90186ec0c5e762814654dd65c7
is2js <tingstyle1@gmail.com>
2021-11-13 15:28:20 +0900

    feat: first commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+//		System.out.println(3+4);
+//		System.out.println("덧셈 : " + 3+4); //덧셈 : 34
+		System.out.println("덧셈 : " + (3+4)); //덧셈 : 34 -> 문자열과 더하는데, 숫자부터 먼저 계산하고 문자열로 변환되도록 괄호를 먼저 치자.
+		System.out.println("뺄셈 : " + (4-3)); //덧셈 : 34 -> 문자열과 더하는데, 숫자부터 먼저 계산하고 문자열로 변환되도록 괄호를 먼저 치자.
+		System.out.println("곱셈 : " + (4*3)); //덧셈 : 34 -> 문자열과 더하는데, 숫자부터 먼저 계산하고 문자열로 변환되도록 괄호를 먼저 치자.
+		System.out.println("나눗셈 : " + (4/3)); //덧셈 : 34 -> 문자열과 더하는데, 숫자부터 먼저 계산하고 문자열로 변환되도록 괄호를 먼저 치자.
+		
+
+	}
+}
