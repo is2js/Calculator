09186871657ef5ff9f14ff6ba1a1e629297617f4
is2js <tingstyle1@gmail.com>
2021-11-14 11:42:55 +0900

    문자열계산기 1차 완성

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,31 +2,38 @@ import java.util.Scanner;
 
 public class StringCalculator {
 	public static void main(String[] args) {
-		//1. 처음 만들 건 스캐너 만들고, 사용자에게 뭐하는지 알려주기  + 자동 import는 ctrl+1이다.
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("계산할 값 입력 : ");
 		String value = scanner.nextLine();
 		System.out.println("입력 값 : " + value);
 		
-		//2. 공백기준으로 split해서 배열로 받고, 앞서작성한 caculator에서 로직보기
 		String[] values = value.split(" " ); 
-		// 첫번째값꺼내기 -> 조건에 만족하면 (일단 드가서, quit아니면) symbol과 두번째값은 반복해서 꺼내오면서, 연산하여, 첫번째 값을 업데이트
-		// - quit들어올때까지 무한반복 while True -> 배열에 담긴 것은 [index기준  있을때까지 반복] !! while ( < arr.length)로 바뀜.
-		// - arr.length는 마지막 index(n-1) +1 값이라서.. 기준index가 필요하다 int i =? -> 2번째값부터 꺼내야하니 i=1부터 시작시킨다. 
-		// -> 배열의 끝  while < arr.length(N)으로서 n-1까지!(횟수아님)
 		int first = Integer.parseInt(values[0]);
 		System.out.println("first : " + first);
-		//while ( < values.length) 
+		
 		int i = 1;
+		//6. calculate()메서드는 가져가서 사용할 예정. 우리는 main메서드의 로직만 모방한다.(Calculator의 메서드는 사용. Main만 바꾸는 과정인듯?)
+		// first -> for문위변수result에 [최초의first]주입후, 반복문에서 [직전까지의fisrt] -> [현재 업데이트된 first]로 계속 업데이트 되도록 한다.
+		int result = first;
 		while( i < values.length) {
-			//3. 2번째부터라면.. 첫번째로 꺼내지는 것이 심볼이다. 
 			String symbol = values[i];
 			System.out.println("symbol : " + symbol);
 			
-			// 4. i기준 i=symbol, i+1 다음것=second를 꺼낸다 --> 2개씩 건너뛸 예정?
 			int second= Integer.parseInt(values[i+1]); 
 			System.out.println("second : " + second);
+			
+			//7. 전에 만들어둔 Cacluator 클래스에서 main만 무시하고 메서드는 쓴다.
+			// first, symbol, second를 넣어주고, 업데이트된 first를 int로 반환받는다.  static int calculate(int first, String symbol, int second)
+			result = Calculator.calculate(result, symbol, second);
+			
+			//8. 무한루프도는 이유는 기준i를 증가안시켜서 그렇다.
+			//** i기준으로 2개씩(i, i+1) 뽑았다면, 그다음 값은 i+2에서 2개를 뽑아야한다. 
+			i = i + 2;
 		}
+		
+		//9. Output도 기존 메서드를 활용하면 된다. input -> Main만 달라졌다.
+		Output.print(result);
+		
 	}
 
 }
